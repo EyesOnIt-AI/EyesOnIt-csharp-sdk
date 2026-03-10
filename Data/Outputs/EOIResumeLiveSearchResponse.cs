e84cdbf6eef47c53a0a5ac7d7c72310a5cf37fcf
Amanda Unger <aunger@mosaicatm.com>
2026-03-10 10:33:48 -0400

    Adding pause and resume methods for live searches

---
+++ Data/Outputs/EOIResumeLiveSearchResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace EyesOnItSDK.Data.Outputs
+{
+    public class EOIResumeLiveSearchResponse: EOIBaseOutputs
+    {
+        internal EOIResumeLiveSearchResponse(EOIMessage eoiMessage) : base(eoiMessage)
+        {
+        }
+
+        internal EOIResumeLiveSearchResponse(EOIResponse eoiResponse) : base(eoiResponse)
+        {
+        }
+
+        internal EOIResumeLiveSearchResponse(bool success, string message = null) : base(success, message)
+        {
+        }
+    }
+}
